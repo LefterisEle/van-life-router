b9ed9adf8d53fcf42853e964d463d90d720a705e
LefterisEle <lefteriselefte@gmail.com>
2023-11-26 00:49:01 +0200

    more work done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,13 @@
-import { NavLink, Link } from 'react-router-dom';
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-const Header = () => {
-    const activeStyle = {
-        'font-weight': 'bold',
-        'text-decoration': 'underline',
+export default function Header() {
+    const activeStyles = {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
         color: '#161616',
     };
+
     return (
         <header>
             <Link className='site-logo' to='/'>
@@ -13,26 +15,30 @@ const Header = () => {
             </Link>
             <nav>
                 <NavLink
-                    to='/host'
-                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    to='host'
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
                 >
                     Host
                 </NavLink>
                 <NavLink
-                    to='/about'
-                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    to='about'
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
                 >
                     About
                 </NavLink>
                 <NavLink
-                    to='/vans'
-                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    to='vans'
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
                 >
                     Vans
                 </NavLink>
+                <Link to='login' className='login-link'>
+                    <img
+                        src='/assets/images/avatar-icon.png'
+                        className='login-icon'
+                    />
+                </Link>
             </nav>
         </header>
     );
-};
-
-export default Header;
+}
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,24 +1,31 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useSearchParams, useLoaderData } from 'react-router-dom';
+import { getVans } from '../../api';
 
-const Vans = () => {
-    const [vansInfo, setVansInfo] = useState([]);
-    useEffect(() => {
-        async function fetchVans() {
-            try {
-                const res = await fetch('./api/vans');
-                const data = await res.json();
-                setVansInfo(data.vans);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchVans();
-    }, []);
+export function loader() {
+    return getVans();
+}
 
-    const vanElements = vansInfo.map((van) => (
+export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [error, setError] = React.useState(null);
+    const vans = useLoaderData();
+
+    const typeFilter = searchParams.get('type');
+
+    const displayedVans = typeFilter
+        ? vans.filter((van) => van.type === typeFilter)
+        : vans;
+
+    const vanElements = displayedVans.map((van) => (
         <div key={van.id} className='van-tile'>
-            <Link to={`/vans/${van.id}`}>
+            <Link
+                to={van.id}
+                state={{
+                    search: `?${searchParams.toString()}`,
+                    type: typeFilter,
+                }}
+            >
                 <img src={van.imageUrl} />
                 <div className='van-info'>
                     <h3>{van.name}</h3>
@@ -31,12 +38,58 @@ const Vans = () => {
             </Link>
         </div>
     ));
+
+    function handleFilterChange(key, value) {
+        setSearchParams((prevParams) => {
+            if (value === null) {
+                prevParams.delete(key);
+            } else {
+                prevParams.set(key, value);
+            }
+            return prevParams;
+        });
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>;
+    }
+
     return (
         <div className='van-list-container'>
-            <h1>Expore our van options</h1>
+            <h1>Explore our van options</h1>
+            <div className='van-list-filter-buttons'>
+                <button
+                    onClick={() => handleFilterChange('type', 'simple')}
+                    className={`van-type simple 
+                        ${typeFilter === 'simple' ? 'selected' : ''}`}
+                >
+                    Simple
+                </button>
+                <button
+                    onClick={() => handleFilterChange('type', 'luxury')}
+                    className={`van-type luxury 
+                        ${typeFilter === 'luxury' ? 'selected' : ''}`}
+                >
+                    Luxury
+                </button>
+                <button
+                    onClick={() => handleFilterChange('type', 'rugged')}
+                    className={`van-type rugged 
+                        ${typeFilter === 'rugged' ? 'selected' : ''}`}
+                >
+                    Rugged
+                </button>
+
+                {typeFilter ? (
+                    <button
+                        onClick={() => handleFilterChange('type', null)}
+                        className='van-type clear-filters'
+                    >
+                        Clear filter
+                    </button>
+                ) : null}
+            </div>
             <div className='van-list'>{vanElements}</div>
         </div>
     );
-};
-
-export default Vans;
+}
