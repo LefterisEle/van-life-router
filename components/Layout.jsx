56fab7531d02204df56e80df262776b03259846b
LefterisEle <lefteriselefte@gmail.com>
2023-11-25 14:40:42 +0200

    practising_more_with_react_router

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,4 @@
+import Footer from './Footer';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
 
@@ -5,7 +6,10 @@ const Layout = () => {
     return (
         <>
             <Header />
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </>
     );
 };
